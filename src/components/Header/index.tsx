ede73b00ad8ebae4a8101ca18f70d6666b183357
aksel26 <kevinxkim2023@gmail.com>
2025-03-02 03:13:03 +0900

    Header 컴포넌트

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import * as S from "./styled"
+
+const Header = () => {
+  return (
+    <S.Wrapper>
+      <S.Header>헤더</S.Header>
+    </S.Wrapper>
+  )
+}
+
+export default Header
