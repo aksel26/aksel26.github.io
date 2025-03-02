288ce53397c40764a6fe14eeeb89153675bfe2ad
aksel26 <kevinxkim2023@gmail.com>
2025-03-02 20:51:54 +0900

    글 목록 컴포넌트

---
+++ src/components/FeatureCardWrapper/index.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const FeatureCardWrapper = ({ children }: any) => {
+  return <div className="relative">{children}</div>
+}
+
+export default FeatureCardWrapper
---
+++ src/components/FeaturedCard/styled.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+// import { contentMaxWidth, MOBILE_MEDIA_QUERY } from "./const"
+
+export const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+`
