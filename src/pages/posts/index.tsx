853db351ecc323937ed3b0bc65413c1c12450ab1
aksel26 <kevinxkim2023@gmail.com>
2025-03-06 17:18:52 +0900

    tab 컴포넌트 개발

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,17 +1,39 @@
 import CardComponent from "../../components/CardComponent"
 import Layout from "../../layout"
 // import {CardWrapper} from "./styled"
-import React from "react"
+import React, { useState } from "react"
 import CardWrapper from "./styled"
 
 const Posts = () => {
+  const [activeTab, setActiveTab] = useState(0)
+  console.log("🚀 ~ Posts ~ activeTab:", activeTab)
+
+  const tabs = [
+    { id: 0, label: "All", content: "첫 번째 탭의 내용입니다." },
+    { id: 1, label: "Dev", content: "두 번째 탭의 내용입니다." },
+    { id: 2, label: "Life", content: "세 번째 탭의 내용입니다." },
+  ]
   return (
     <Layout>
-      <h1 className="text-4xl font-bold my-10 tracking-wider">Posts::life</h1>
-      <div className="flex gap-x-4 justify-start">
-        <div>전체</div>
-        <div>개발</div>
-        <div>일상</div>
+      <h1 className="text-4xl font-bold my-10 tracking-wider">
+        Posts.{tabs[activeTab].label}
+      </h1>
+      <div className="flex  transition-all duration-200">
+        <div className="flex gap-x-4 justify-start">
+          {tabs.map(tab => (
+            <button
+              key={tab.id}
+              onClick={() => setActiveTab(tab.id)}
+              className={`py-1 px-2 font-sm text-sm transition-all duration-200 w-14 ${
+                activeTab === tab.id
+                  ? " bg-black text-white"
+                  : " text-gray-500 hover:text-gray-700 hover:bg-gray-100"
+              }`}
+            >
+              {tab.label}
+            </button>
+          ))}
+        </div>
       </div>
       <CardWrapper>
         <ul className="">
