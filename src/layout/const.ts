cc64e20bcfedfdd9de0ee480e392bc32cb06b530
aksel26 <kevinxkim2023@gmail.com>
2025-03-09 03:51:07 +0900

    전체너비 수정

--- src/layout/const.ts
+++ src/layout/const.ts
@@ -1,4 +1,4 @@
-export const contentMaxWidth = "1376px"
+export const contentMaxWidth = "1140px"
 
-export const MOBILE_MAX_WIDTH = 1919
+export const MOBILE_MAX_WIDTH = 1140
 export const MOBILE_MEDIA_QUERY = `screen and (max-width: ${MOBILE_MAX_WIDTH}px)`
