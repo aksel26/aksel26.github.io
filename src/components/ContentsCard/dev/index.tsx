0a5d80bae67f3eb374af73624377f21d265a12e8
aksel26 <kevinxkim2023@gmail.com>
2025-03-09 18:24:54 +0900

    반응형

--- src/components/ContentsCard/dev/index.tsx
+++ src/components/ContentsCard/dev/index.tsx
@@ -4,39 +4,61 @@ import * as S from "../styled"
 
 const ContensCardDev = () => {
   return (
-    <div className=" md:flex gap-x-12 mt-20">
-      <div className="md:w-[54%]">
+    <div className="flex flex-col md:flex-row gap-y-12  md:gap-x-12 mt-20 px-8 md:px-0">
+      <div className="md:w-[54%] ">
         <CardWrapper>
           <div className="flex flex-col gap-y-3">
             <img
               src="https://picsum.photos/384/250"
               alt="thumbnailImg"
-              className=" h-[300px] rounded-sm"
+              className="h-[240px] md:h-[300px] rounded-sm"
             />
-            <div className="font-bold text-2xl line-clamp-1">
+            <div className="font-bold text-xl md:text-2xl line-clamp-1">
               모든 국민은 근로의 권리를 가진다. 국가는 사회적·경제적 방법으로
               근로자의 고용의 증진과 적정임금의 보장에 노력하여야 하며,
             </div>
-            <div className="flex justify-between text-sm text-slate-400 ">
+            <p className="font-normal text-sm md:text-base line-clamp-2 text-slate-400 ">
+              모든 국민은 근로의 권리를 가진다. 국가는 사회적·경제적 방법으로
+              근로자의 고용의 증진과 적정임금의 보장에 노력하여야 하며,모든
+              국민은 근로의 권리를 가진다. 국가는 사회적·경제적 방법으로
+              근로자의 고용의 증진과 적정임금의 보장에 노력하여야 하며,근로자의
+              고용의 증진과 적정임금의 보장에 노력하여야 하며,근로자의 고용의
+              증진과 적정임금의 보장에 노력하여야 하며,근로자의 고용의 증진과
+              적정임금의 보장에 노력하여야 하며,근로자의 고용의 증진과
+              적정임금의 보장에 노력하여야 하며,근로자의 고용의 증진과
+              적정임금의 보장에 노력하여야 하며,
+            </p>
+            <div className="flex justify-between text-xs md:text-sm text-slate-400 ">
               <p>2025.02.05</p>
               <p>Dev</p>
             </div>
           </div>
         </CardWrapper>
       </div>
-      <div className="flex-1">
+      <div className="flex-1 md:mt-0">
         <CardWrapper>
           <div className="flex flex-col gap-y-3">
             <img
               src="https://picsum.photos/384/250"
               alt="thumbnailImg"
-              className=" h-[300px] rounded-sm"
+              className="h-[240px] md:h-[300px] rounded-sm"
             />
-            <div className="font-bold text-2xl line-clamp-1">
+            <div className="font-bold text-xl md:text-2xl line-clamp-1">
               모든 국민은 근로의 권리를 가진다. 국가는 사회적·경제적 방법으로
               근로자의 고용의 증진과 적정임금의 보장에 노력하여야 하며,
             </div>
-            <div className="flex justify-between text-sm text-slate-400 ">
+            <p className="font-normal text-sm md:text-base line-clamp-2 text-slate-400 ">
+              모든 국민은 근로의 권리를 가진다. 국가는 사회적·경제적 방법으로
+              근로자의 고용의 증진과 적정임금의 보장에 노력하여야 하며,모든
+              국민은 근로의 권리를 가진다. 국가는 사회적·경제적 방법으로
+              근로자의 고용의 증진과 적정임금의 보장에 노력하여야 하며,근로자의
+              고용의 증진과 적정임금의 보장에 노력하여야 하며,근로자의 고용의
+              증진과 적정임금의 보장에 노력하여야 하며,근로자의 고용의 증진과
+              적정임금의 보장에 노력하여야 하며,근로자의 고용의 증진과
+              적정임금의 보장에 노력하여야 하며,근로자의 고용의 증진과
+              적정임금의 보장에 노력하여야 하며,
+            </p>
+            <div className="flex justify-between text-xs md:text-sm text-slate-400 ">
               <p>2025.02.05</p>
               <p>Dev</p>
             </div>
