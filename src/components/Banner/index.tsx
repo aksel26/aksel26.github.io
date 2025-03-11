fa2b8d7840378a5fc7e6855a89a0f2a010f66366
aksel26 <kevinxkim2023@gmail.com>
2025-03-11 20:56:44 +0900

    텍스트 추가

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -2,9 +2,20 @@ import React from "react"
 
 const Banner = () => {
   return (
-    <div className="w-full pt-24 bg-slate-50 pb-12">
+    <div className="w-full pt-24 bg-slate-100 pb-12 px-8">
       <h1 className="text-4xl font-bold mb-3">HM-Blog</h1>
-      <h1 className="text-xl  text-slate-500">개발자 일상을 기록합니다</h1>
+
+      <div className="flex flex-col gap-y-3">
+        <p className="text-base  text-gray-400">
+          코드와 커피 향 사이를 거닐며 순간을 기록하는{" "}
+          <span className="text-gray-800">프론트엔드 개발자</span>입니다.
+        </p>
+
+        <p className="text-gray-400">
+          코드처럼 깔끔하고 오류 없는 일상을 '디버깅'하듯, 취미와 개발을
+          조화롭게 균형을 잡아가는 삶을 기록합니다.
+        </p>
+      </div>
     </div>
   )
 }
