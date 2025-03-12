c14ff983dfdd4d2135c6079d698853b476bd98a3
aksel26 <kevinxkim2023@gmail.com>
2025-03-12 16:34:31 +0900

    mobile sticky

--- src/components/FeaturedCard/daily/wrapper/index.tsx
+++ src/components/FeaturedCard/daily/wrapper/index.tsx
@@ -4,49 +4,33 @@ interface TCardWrapper {
   category: string
   contents: any[]
 }
-import { ChevronRight } from "lucide-react"
 
 export const VerticalCardWrapper = ({ category, contents }: TCardWrapper) => {
   const categoryLower = category.toLowerCase()
 
   return (
     <div className="md:flex gap-x-10 items-start mt-20  ">
-      <div className="md:w-[35%] bg-slate-100 h-full relative md:sticky md:top-48">
+      <div className="md:w-[35%] bg-slate-100 h-full relative md:sticky md:top-48 sticky top-14 z-10">
         {categoryLower === "travel" && (
-          <div className="relative">
-            <StaticImage
-              src="../../../../images/travel.jpg"
-              alt="travel"
-              className="opacity-75 h-32 sm:h-32 md:h-96"
-            />
-            <span className="absolute right-6 bottom-4 bg-white p-2 rounded-sm opacity-50 hover:bg-black transition cursor-pointer group">
-              <ChevronRight className="text-[oklch(0.274_0.006_286.033)] group-hover:text-white transition" />
-            </span>
-          </div>
+          <StaticImage
+            src="../../../../images/travel.jpg"
+            alt="travel"
+            className="opacity-75 h-32 sm:h-32 md:h-96 "
+          />
         )}
         {categoryLower === "food" && (
-          <div className="relative">
-            <StaticImage
-              src="../../../../images/cafe.jpg"
-              alt="cafe"
-              className="opacity-75 h-32 sm:h-32 md:h-96"
-            />
-            <span className="absolute right-6 bottom-4 bg-white p-2 rounded-sm opacity-50 hover:bg-black transition cursor-pointer group">
-              <ChevronRight className="text-[oklch(0.274_0.006_286.033)] group-hover:text-white transition" />
-            </span>
-          </div>
+          <StaticImage
+            src="../../../../images/cafe.jpg"
+            alt="cafe"
+            className="opacity-75 h-32 sm:h-32 md:h-96"
+          />
         )}
         {categoryLower === "etc" && (
-          <div className="relative">
-            <StaticImage
-              src="../../../../images/etc.jpg"
-              alt="etc"
-              className="opacity-75 h-32 sm:h-32 md:h-96"
-            />
-            <span className="absolute right-6 bottom-4 bg-white p-2 rounded-sm opacity-50 hover:bg-black transition cursor-pointer group">
-              <ChevronRight className="text-[oklch(0.274_0.006_286.033)] group-hover:text-white transition" />
-            </span>
-          </div>
+          <StaticImage
+            src="../../../../images/etc.jpg"
+            alt="etc"
+            className="opacity-75 h-32 sm:h-32 md:h-96"
+          />
         )}
 
         <p className="absolute bottom-3 left-8 text-white font-bold  text-6xl md:text-8xl tracking-wider">
