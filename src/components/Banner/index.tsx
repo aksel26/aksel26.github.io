179d4aaccca27dce3409e9db04e437696abca83b
aksel26 <kevinxkim2023@gmail.com>
2025-03-13 16:31:15 +0900

    Banner css

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -3,12 +3,12 @@ import React from "react"
 const Banner = () => {
   return (
     <div className="w-full pt-24 bg-slate-100 pb-12 px-8">
-      <h1 className="text-4xl font-bold mb-3">HM-Blog</h1>
+      <h1 className="text-4xl font-extrabold mb-3">HM-Blog</h1>
 
       <div className="flex flex-col gap-y-3">
-        <p className="text-base  text-gray-400">
+        {/* <p className="text-base  text-gray-400">
           <span className="text-gray-800">프론트엔드 개발자</span>입니다.
-        </p>
+        </p> */}
 
         <p className="text-gray-400">
           코드처럼 깔끔하고 오류 없는 일상을 '디버깅'하듯, 취미와 개발을
