061bf40d0d8417109ed51462033a6a62593a4ab2
aksel26 <kevinxkim2023@gmail.com>
2025-03-13 16:33:15 +0900

    scroll-behavior global css

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -8,6 +8,8 @@ const GlobalStyle = createGlobalStyle`
     font-family: 'Pretendard', -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
     margin: 0;
     padding: 0;
+    scroll-behavior: smooth;
+
   }
   
   * {
