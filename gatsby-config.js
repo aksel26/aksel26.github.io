e3308d844a70f5f8887af0a16613c00cabadacda
aksel26 <kevinxkim2023@gmail.com>
2025-03-16 22:32:31 +0900

    gatsby-plugin-sharp

--- gatsby-config.js
+++ gatsby-config.js
@@ -142,5 +142,15 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        defaults: {
+          formats: ["auto", "webp"],
+          quality: 100,
+          placeholder: "blurred",
+        },
+      },
+    },
   ],
 }
