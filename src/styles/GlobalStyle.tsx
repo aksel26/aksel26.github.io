41447bfa3e8300bb73839f6d3a8798af47705a16
aksel26 <kevinxkim2023@gmail.com>
2025-03-20 18:48:31 +0900

    scroll css

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -2,13 +2,14 @@
 import { createGlobalStyle } from "styled-components"
 
 const GlobalStyle = createGlobalStyle`
-  
+  html {
+    scroll-behavior: smooth;
+  }
   
   body {
     font-family: 'Pretendard', -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
     margin: 0;
     padding: 0;
-    scroll-behavior: smooth;
 
   }
   
