6cf397f48887c10d50a4ece6c2a67f28b03367bd
aksel26 <kevinxkim2023@gmail.com>
2025-03-20 18:49:30 +0900

    Dev 내용 분류

--- src/components/MainCard/dev/index.tsx
+++ src/components/MainCard/dev/index.tsx
@@ -3,72 +3,42 @@ import { Link } from "gatsby"
 import { GatsbyImage } from "gatsby-plugin-image"
 
 const DevContainer = ({ posts }: any) => {
+  const mostCurrent = posts.slice(0, 2)
+  const rest = posts.slice(2)
+  console.log("🚀 ~ DevContainer ~ posts:", posts)
   return (
     <section className="mt-0">
       <div className="flex flex-col md:flex-row gap-y-12  md:gap-x-5 px-8 md:px-0">
-        <Link className="md:w-[54%]" to="#">
-          <div className="flex flex-col gap-y-3 pointer-cursor group">
-            <div className="h-[240px] md:h-[300px] overflow-hidden rounded-sm">
-              <img
-                src="https://picsum.photos/384/250"
-                alt="thumbnailImg"
-                className="w-full h-full object-cover transition-all duration-300 ease-in-out group-hover:scale-110	"
-              />
-            </div>
-            <div className="title font-bold text-xl md:text-2xl line-clamp-1 transition-all duration-300 ease-in-out group-hover:text-blue-700 ">
-              모든 국민은 근로의 권리를 가진다. 국가는 사회적·경제적 방법으로
-              근로자의 고용의 증진과 적정임금의 보장에 노력하여야 하며,
-            </div>
-            <p className="font-normal text-sm md:text-base line-clamp-2 text-slate-400 ">
-              모든 국민은 근로의 권리를 가진다. 국가는 사회적·경제적 방법으로
-              근로자의 고용의 증진과 적정임금의 보장에 노력하여야 하며,모든
-              국민은 근로의 권리를 가진다. 국가는 사회적·경제적 방법으로
-              근로자의 고용의 증진과 적정임금의 보장에 노력하여야 하며,근로자의
-              고용의 증진과 적정임금의 보장에 노력하여야 하며,근로자의 고용의
-              증진과 적정임금의 보장에 노력하여야 하며,근로자의 고용의 증진과
-              적정임금의 보장에 노력하여야 하며,근로자의 고용의 증진과
-              적정임금의 보장에 노력하여야 하며,근로자의 고용의 증진과
-              적정임금의 보장에 노력하여야 하며,
-            </p>
-            <div className="flex justify-between text-xs md:text-sm text-slate-400 ">
-              <p>2025.02.05</p>
-              <p>Dev</p>
-            </div>
-          </div>
-        </Link>
-        <Link className="flex-1 md:mt-0" to="#">
-          <div className="flex flex-col gap-y-3 pointer-cursor group">
-            <div className="h-[240px] md:h-[300px] overflow-hidden rounded-sm">
-              <img
-                src="https://picsum.photos/384/250"
-                alt="thumbnailImg"
-                className="w-full h-full object-cover transition-all duration-300 ease-in-out group-hover:scale-110	"
-              />
-            </div>
-            <div className="title font-bold text-xl md:text-2xl line-clamp-1 transition-all duration-300 ease-in-out group-hover:text-blue-700 ">
-              모든 국민은 근로의 권리를 가진다. 국가는 사회적·경제적 방법으로
-              근로자의 고용의 증진과 적정임금의 보장에 노력하여야 하며,
-            </div>
-            <p className="font-normal text-sm md:text-base line-clamp-2 text-slate-400 ">
-              모든 국민은 근로의 권리를 가진다. 국가는 사회적·경제적 방법으로
-              근로자의 고용의 증진과 적정임금의 보장에 노력하여야 하며,모든
-              국민은 근로의 권리를 가진다. 국가는 사회적·경제적 방법으로
-              근로자의 고용의 증진과 적정임금의 보장에 노력하여야 하며,근로자의
-              고용의 증진과 적정임금의 보장에 노력하여야 하며,근로자의 고용의
-              증진과 적정임금의 보장에 노력하여야 하며,근로자의 고용의 증진과
-              적정임금의 보장에 노력하여야 하며,근로자의 고용의 증진과
-              적정임금의 보장에 노력하여야 하며,근로자의 고용의 증진과
-              적정임금의 보장에 노력하여야 하며,
-            </p>
-            <div className="flex justify-between text-xs md:text-sm text-slate-400 ">
-              <p>2025.02.05</p>
-              <p>Dev</p>
-            </div>
-          </div>
-        </Link>
+        {mostCurrent.map(
+          ({ node: { frontmatter, fields } }: any, index: number) => (
+            <Link className="md:w-[54%]" to={fields.slug} key={index}>
+              <div className="flex flex-col gap-y-3 pointer-cursor group">
+                <div className="h-[240px]  md:h-[300px] overflow-hidden rounded-sm relative">
+                  <GatsbyImage
+                    image={
+                      frontmatter.thumbnail.childImageSharp.gatsbyImageData
+                    }
+                    alt="thumbnailImg"
+                    className="w-full h-full object-cover absolute inset-0 transition-all duration-300 ease-in-out group-hover:scale-110	"
+                  />
+                </div>
+                <div className="title font-bold text-xl md:text-2xl line-clamp-1 transition-all duration-300 ease-in-out group-hover:text-blue-700 ">
+                  {frontmatter.title}
+                </div>
+                <p className="font-normal text-sm md:text-base line-clamp-2 text-slate-400 ">
+                  {frontmatter.summary}
+                </p>
+                <div className="flex justify-between text-xs md:text-sm text-slate-400 ">
+                  <p>{frontmatter.date}</p>
+                  <p>Dev</p>
+                </div>
+              </div>
+            </Link>
+          )
+        )}
       </div>
       <div className="flex md:flex-row flex-col gap-y-12 gap-x-5 mt-12 px-8 md:px-0 w-full">
-        {posts.map(
+        {rest.map(
           (
             {
               node: {
