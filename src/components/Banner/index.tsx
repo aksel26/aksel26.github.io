8df4bd64325c6e90ce6888c22491e84581917d3b
aksel26 <kevinxkim2023@gmail.com>
2025-03-22 18:53:52 +0900

    Banner 컴포넌ㄴ트

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,16 +1,57 @@
 import React from "react"
-
-const Banner = ({ siteTitle }: any) => {
+import { motion } from "framer-motion"
+const Banner = ({ siteTitle, currentCategory, selectCategory }: any) => {
   return (
-    <div className="w-full pt-24 pb-12 px-8 text-white">
-      <h1 className="text-xl font-extrabold mb-3">{siteTitle}</h1>
-
-      {/* <div className="flex flex-col gap-y-3">
-        <p className="text-gray-400">
-          코드처럼 깔끔하고 오류 없는 일상을 '디버깅'하듯, 취미와 개발을
-          조화롭게 균형을 잡아가는 삶을 기록합니다.
-        </p>
-      </div> */}
+    <div className="relative text-white mt-48">
+      <div className="absolute bottom-0 left-0 right-0 h-[0.5px]">
+        <motion.div
+          className="h-full bg-gray-100 dark:bg-gray-700"
+          initial={{ width: "0%" }}
+          animate={{ width: "100%" }}
+          transition={{ duration: 2, ease: "easeInOut" }}
+        />
+      </div>
+      <motion.p
+        initial={{ opacity: 0, y: -50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, ease: "easeOut" }}
+        className="absolute left-12 -top-8 font-black"
+      >
+        {siteTitle}
+      </motion.p>
+      <ul className="flex gap-x-24 absolute left-1/2 -top-8 font-black cursor-pointer">
+        <motion.li
+          initial={{ opacity: 0, y: -50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, ease: "easeOut", delay: 0.8 }}
+          whileHover={{
+            x: -5,
+            transition: {
+              duration: 0.2,
+              delay: 0,
+              exit: { duration: 0.4, delay: 0 },
+            },
+          }}
+          className={`${
+            currentCategory === 1 ? "text-red-400" : "text-white"
+          } `}
+          onClick={() => selectCategory(1)}
+        >
+          Dev
+        </motion.li>
+        <motion.li
+          initial={{ opacity: 0, y: -50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, ease: "easeOut", delay: 1.3 }}
+          whileHover={{ x: -5 }}
+          onClick={() => selectCategory(2)}
+          className={`${
+            currentCategory === 2 ? "text-red-400" : "text-white"
+          } `}
+        >
+          LifeLog
+        </motion.li>
+      </ul>
     </div>
   )
 }
