b9c35da3c80bb272bc904b057b0b2451f9b4b48d
aksel26 <kevinxkim2023@gmail.com>
2025-03-25 09:56:50 +0900

    mp4 type추가

---
+++ src/types/custom.d.ts
@@ -0,0 +1,14 @@
+declare module "*.mp4" {
+  const src: string
+  export default src
+}
+
+declare module "*.jpg" {
+  const content: string
+  export default content
+}
+
+declare module "*.png" {
+  const content: string
+  export default content
+}
