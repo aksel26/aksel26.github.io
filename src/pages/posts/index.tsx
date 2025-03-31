cb3d7503a7c708c3dcbbfb42dcc6bf751c2daa98
aksel26 <kevinxkim2023@gmail.com>
2025-03-31 17:06:48 +0900

    카테고리 선택

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,33 +1,78 @@
 import CardComponent from "../../components/CardComponent"
 import Layout from "../../layout"
 // import {CardWrapper} from "./styled"
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import CardWrapper from "./styled"
 import { graphql, Link } from "gatsby"
-
+import { AnimatePresence, motion } from "framer-motion"
 const Posts = ({ data }: any) => {
   const [activeTab, setActiveTab] = useState(0)
-  console.log("🚀 ~ Posts ~ activeTab:", activeTab)
   const posts = data?.allMarkdownRemark.edges
 
+  const [result, setResult] = useState([])
   const tabs = [
     { id: 0, label: "All", content: "첫 번째 탭의 내용입니다." },
     { id: 1, label: "Dev", content: "두 번째 탭의 내용입니다." },
-    { id: 2, label: "Life", content: "세 번째 탭의 내용입니다." },
+    { id: 2, label: "LifeLog", content: "세 번째 탭의 내용입니다." },
   ]
+
+  useEffect(() => {
+    // posts가 undefined일 경우 빈 배열로 처리
+    const postsData = posts || []
+
+    const filteredPosts =
+      activeTab === 0
+        ? postsData
+        : postsData.filter((post: any) => {
+            const category = tabs.find(tab => tab.id === activeTab)?.label
+            return post.node.frontmatter.mainCategory === category
+          })
+
+    setResult(filteredPosts)
+  }, [activeTab, posts]) // 의존성 배열에 activeTab과 posts 추
+
   return (
     <Layout>
       <div className="pt-20 text-white">
-        <h1 className="text-xl font-semibold py-5 tracking-wider px-8">
-          Posts.{tabs[activeTab].label}
-        </h1>
+        <div className="flex gap-x-2 text-xl font-semibold py-5 tracking-wider px-8">
+          <motion.div className="flex items-baseline gap-2">
+            <AnimatePresence mode="wait">
+              <motion.h1
+                key={result.length} // key를 변경하여 값이 바뀔 때마다 새 요소로 인식
+                initial={{ opacity: 0, y: -20 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: 20 }}
+                transition={{ duration: 0.7 }}
+                className="min-w-6"
+              >
+                {result.length}
+              </motion.h1>
+            </AnimatePresence>
+
+            <div className="flex">
+              <h1>Posts.</h1>
+              <AnimatePresence mode="wait">
+                <motion.h1
+                  key={tabs[activeTab].label} // activeTab이 변경될 때마다 새 요소로 인식
+                  initial={{ opacity: 0, y: -20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: 20 }}
+                  transition={{ duration: 0.4 }}
+                >
+                  {tabs[activeTab].label}
+                </motion.h1>
+              </AnimatePresence>
+            </div>
+          </motion.div>
+        </div>
+
         <div className="flex  transition-all duration-200 py-4 px-8">
           <div className="flex gap-x-4 justify-start">
             {tabs.map(tab => (
               <button
                 key={tab.id}
                 onClick={() => setActiveTab(tab.id)}
-                className={`py-1 px-2 font-sm text-sm transition-all duration-200 w-14 ${
+                className={`py-1 px-2 font-sm text-sm transition-all duration-200 min-w-20 ${
                   activeTab === tab.id
                     ? " bg-black text-white"
                     : " text-gray-500 hover:text-gray-700 hover:bg-gray-100"
@@ -40,7 +85,7 @@ const Posts = ({ data }: any) => {
         </div>
         <CardWrapper>
           <ul className="flex gap-10 md:gap-4 flex-wrap">
-            {posts.map(
+            {result.map(
               ({
                 node: {
                   fields: { slug },
