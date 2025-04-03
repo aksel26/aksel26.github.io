773124250080d125bd5994f1e2c141f95b4e6e13
aksel26 <kevinxkim2023@gmail.com>
2025-04-03 12:01:35 +0900

    css

--- src/components/MainCard/dev/index.tsx
+++ src/components/MainCard/dev/index.tsx
@@ -15,7 +15,7 @@ const SkeletonComponent = () => {
 
 const DevContainer = ({ posts }: any) => {
   return (
-    <section className="mt-0 max-w-[1024px] mx-auto h-full py-24 ">
+    <section className="mt-0 max-w-[1024px] mx-auto h-full py-20 ">
       <div
         id="contents"
         className="md:h-[400px] gap-y-16 gap-x-10 flex flex-col md:flex-wrap md:px-0 px-6"
